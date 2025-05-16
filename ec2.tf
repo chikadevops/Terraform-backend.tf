776b7af1467771a3d8638ce2b415ce70ab7f685c
Chikadevops <chikadevops@gmail.com>
2025-05-16 18:30:33 +0100

    EC2 commit

--- ec2.tf
+++ ec2.tf
@@ -1,8 +1,8 @@
 resource "aws_instance" "demo_instance" {
-    ami              = "ami-084568db4383264d4"
-    instance_type    = "t2.micro"
+  ami           = "ami-084568db4383264d4"
+  instance_type = "t2.micro"
 
-    tags        = {
-        name    = "Terraform_EC2"
-    }
+  tags = {
+    name = "Terraform_EC2"
+  }
 }
